4237e28bdc7218386fee3f295d4845157fb556db
Byron Kimani <byronkimani@gmail.com>
2020-05-11 17:59:47 +0300

    Added getters and aligned text to center

--- lib/result.dart
+++ lib/result.dart
@@ -5,13 +5,29 @@ class Result extends StatelessWidget {
 
   Result(this.resultScore);
 
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 8) {
+      resultText = 'You are nice and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are ... strange?';
+    } else {
+      resultText = 'You are bad!';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        'You did it man!',
+        resultPhrase,
         style: TextStyle(
             color: Colors.red, fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
       ),
     );
   }
